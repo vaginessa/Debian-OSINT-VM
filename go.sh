043d70a2c994f85a276823423c3a9c844f9b241a
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-17 18:52:07 -0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@ go install github.com/OJ/gobuster/v3@latest
 go install github.com/jaeles-project/gospider@latest
 go install github.com/hakluke/hakrawler@latest
 go install github.com/tomnomnom/httprobe@master
+go install -v github.com/owasp-amass/oam-tools/cmd/...@master
 
 wget https://github.com/sensepost/gowitness/releases/download/2.5.1/gowitness-2.5.1-linux-amd64
 mv gowitness-2.5.1-linux-amd64 ~/go/bin
