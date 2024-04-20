8137ead4636669ac36de30b2d876cc0ee7f1812b
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-20 16:58:09 -0700

    Update debian_pipx_git_osint_tools.sh

--- debian_pipx_git_osint_tools.sh
+++ debian_pipx_git_osint_tools.sh
@@ -181,7 +181,7 @@ git clone https://github.com/BigggChungus/onion-links.git
 git clone https://github.com/01Kevin01/OnionLinksV3.git
 git clone https://github.com/The-Hidden-Wiki-URLs/2024.git
 
-mkdir .config/amass
+mkdir ~/.config/amass
 cd .config/amass
 git clone https://github.com/proabiral/Fresh-Resolvers.git
 cd
