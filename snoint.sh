399aa3aa261a95bf004e14f79d65f6c30899804b
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-20 15:40:41 -0700

    Update snoint.sh

--- snoint.sh
+++ snoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install curl sq
-curl -sSf https://apt.vulns.xyz/kpcyrd.pgp | sq keyring filter -B --handle 64B13F7117D6E07D661BBCE0FE763A64F5E54FD6 | sudo tee /etc/apt/trusted.gpg.d/apt-vulns-xyz.gpg > /dev/null
+curl -sSf https://apt.vulns.xyz/kpcyrd.pgp | sq dearmor | sudo tee /etc/apt/trusted.gpg.d/apt-vulns-xyz.gpg > /dev/null
 echo deb http://apt.vulns.xyz stable main | sudo tee /etc/apt/sources.list.d/apt-vulns-xyz.list
 apt update
 apt install sn0int -y
