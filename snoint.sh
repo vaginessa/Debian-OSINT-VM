2f901b7c1e009f0773ee9e02a9f01366d1acc8ca
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-20 15:42:35 -0700

    Update snoint.sh

--- snoint.sh
+++ snoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install curl sq
+apt install apt-transport-https curl sq 
 curl -sSf https://apt.vulns.xyz/kpcyrd.pgp | sq dearmor | sudo tee /etc/apt/trusted.gpg.d/apt-vulns-xyz.gpg > /dev/null
 echo deb http://apt.vulns.xyz stable main | sudo tee /etc/apt/sources.list.d/apt-vulns-xyz.list
 apt update
